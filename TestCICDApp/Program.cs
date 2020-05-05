107224118897b778ee94ce4ea234f6fa7706c0cf
Ronny Muliawan <ronny.muliawan@lyteventures.com>
2020-05-05 14:44:13 +0800

    Test fresh.

--- TestCICDApp/Program.cs
+++ TestCICDApp/Program.cs
@@ -20,7 +20,7 @@ namespace TestCICDApp
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>(); 
                 });
     }
 }
