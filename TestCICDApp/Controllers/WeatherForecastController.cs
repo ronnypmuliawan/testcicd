e0ba6411a88aa7310af3c92c9a6bb86dcd454f88
Ronny Muliawan <ronny.muliawan@lyteventures.com>
2020-05-05 17:22:31 +0800

    Test S3 IAM Role.

--- TestCICDApp/Controllers/WeatherForecastController.cs
+++ TestCICDApp/Controllers/WeatherForecastController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Amazon.S3;
+using Amazon.S3.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -11,6 +13,8 @@ namespace TestCICDApp.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
+        private readonly IAmazonS3 _client;
+
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
@@ -18,9 +22,10 @@ namespace TestCICDApp.Controllers
 
         private readonly ILogger<WeatherForecastController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IAmazonS3 client)
         {
             _logger = logger;
+            _client = client;
         }
 
         [HttpGet]
@@ -46,6 +51,33 @@ namespace TestCICDApp.Controllers
         public IActionResult Test2()
         {
             return Content("test 2");
+        }
+
+        // test the IAM Role and C# SDK.
+        [HttpGet("iamtest")]
+        public async Task<IActionResult> IamTest([FromQuery] string bucketName, [FromQuery] string key)
+        {
+            try
+            {
+                var request = new GetObjectRequest()
+                {
+                    BucketName = bucketName,
+                    Key = key
+                };
+
+                var response = await _client.GetObjectAsync(request);
+                return File(response.ResponseStream, "image/png");
+            }
+            catch (AmazonS3Exception e)
+            {
+                _logger.LogError(e, e.Message);
+                throw new ArgumentException($"An exception occured when retrieving object from AWS S3 with key name: {key}");
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, e.Message);
+                throw new ArgumentException($"An exception occured when retrieving object from AWS S3 with key name: {key}");
+            }
         }
     }
 }
